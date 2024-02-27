4d6a0b120cc6eecd7c65cedf2d6ec2b562060635
dr-nomark <74582504+dr-nomark@users.noreply.github.com>
2024-02-27 18:03:01 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.chat_models import ChatOpenAI
+# from langchain.embeddings import OpenAIEmbeddings
+# from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage, SystemMessage
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.schema import ChatMessage
