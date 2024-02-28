c3a34ba11c3443d56f159536f38ebaf007594d72
dr-nomark <74582504+dr-nomark@users.noreply.github.com>
2024-02-28 17:49:37 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -133,9 +133,9 @@ def generate_response(query_text, vectorstore, callback):
     
     # chaining
     rag_prompt = [
-        # SystemMessage(
-        #     content="너는 문서에 대해 질의응답을 하는 '씨엔이'야. 주어진 문서를 참고하여 사용자의 질문에 답변을 해줘. 문서에 내용이 정확하게 나와있지 않으면 대답하지 마."
-        # ),
+        AIMessage(
+            content="너는 문서에 대해 질의응답을 하는 '씨엔이'야. 주어진 문서를 참고하여 사용자의 질문에 답변을 해줘. 문서에 내용이 정확하게 나와있지 않으면 대답하지 마."
+        ),
         HumanMessage(
             content=f"질문:{query_text}\n\n{docs}"
         ),
@@ -152,9 +152,9 @@ def generate_summarize(raw_text, callback):
     
     # prompt formatting
     rag_prompt = [
-        # SystemMessage(
-        #     content="다음 나올 문서를 'Notion style'로 요약해줘. 중요한 내용만."
-        # ),
+        AIMessage(
+            content="다음 나올 문서를 'Notion style'로 요약해줘. 중요한 내용만."
+        ),
         HumanMessage(
             content=raw_text
         ),
